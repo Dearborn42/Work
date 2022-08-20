f19dc89a2bd0bc867e9eabef71199af8ffde9abf
Dearborn42 <amurf26@outlook.com>
2022-08-19 17:59:34 -0700

    Stuff
    sad

---
+++ Website.py
@@ -0,0 +1,15 @@
+class Person:
+  def __init__(self, name, grade, points):
+    self.name = name
+    self.grade = grade
+    self.points = points
+
+  def myfunc(x):
+    print(x.name, x.grade, x.points)
+
+
+p1 = Person("John", 'B', 3)
+p2 = Person("Joe", 'A', 6)
+
+Person.myfunc(p1)
+Person.myfunc(p2)

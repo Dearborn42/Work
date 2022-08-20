236a6bd7f3e4fc275515e95aaa0a2987e2131441
Andrew <324737364@student.guhsdaz.org>
2022-08-19 23:57:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 numStudents = 5
 #creates an empty list
 studentList = []
@@ -5,27 +7,36 @@ studentList = []
 #can replace items later, they are just placeholders for now
 
 
-for x in range(0, numStudents):
-    studentList.append(x)
-print(studentList)
-
 class student:
       
    def __init__(self, name, grade, points):
     self.name = name
     self.grade = grade
     self.points = points
+# creates class
 
-s1 = student('HARRY', 'A', 12)
+for x in range(0, numStudents):
+    num = random.randint(0, 10)
+    studentList.append(student('John', 'A', num))
+    print(studentList[x].points)
+
+# turns numStudents into that many classes
+
+s1 = student('Harry', 'A', 12)
 s2 = student('Johnny', 'C', 6)
 s3 = student('Bob', 'B', 3)
 s4 = student('Ryan', 'D', 49)
+
+# Test objects
 
 Points = [s1.points, s2.points, s3.points, s4.points]
 Best = 0
 SecondBest = 0
 ThirdBest = 0
 FourthBest = 0
+
+# test list and varibles
+
 for x in Points:
     if x > Best:
         Best = x
@@ -36,6 +47,7 @@ for z in Points:
     if z > ThirdBest and z != Best and z != SecondBest:
         ThirdBest = z
 
+# Finds highest number first run through, second run through it finds second highest, thrid run through is finds the third highest
 
 print(Best, SecondBest, ThirdBest)
 

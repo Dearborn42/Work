b89ed3a98db9436bdb5da14ecf4c0e32be6f4b79
Andrew <324737364@student.guhsdaz.org>
2022-08-19 21:39:48 -0700

    Have working Object generator and list sorter
    Stuff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+
 class student:
       
    def __init__(self, name, grade, points):
@@ -6,12 +7,25 @@ class student:
     self.grade = grade
     self.points = points
 
-s1 = student('HARRY', 'A', 5)
-s2 = student('Johnny', 'C', 7)
+s1 = student('HARRY', 'A', 12)
+s2 = student('Johnny', 'C', 6)
+s3 = student('Bob', 'B', 3)
+s4 = student('Ryan', 'D', 49)
 
-Points = [s1.points, s2.points]
+Points = [s1.points, s2.points, s3.points, s4.points]
 Best = 0
+SecondBest = 0
+ThirdBest = 0
+FourthBest = 0
+for x in Points:
+    if x > Best:
+        Best = x
+for y in Points:
+    if y > SecondBest and y != Best:
+       SecondBest = y
+for z in Points:
+    if z > ThirdBest and z != Best and z != SecondBest:
+        ThirdBest = z
 
 
-print(s1.name)
-print(s2.grade)
+print(Best, SecondBest, ThirdBest)

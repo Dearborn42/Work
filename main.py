75447f1fffdd58fe851904be5ecf6d5e1f17f6bb
ContriverJake <77342747+JRigney6993-coder@users.noreply.github.com>
2022-08-19 23:49:13 -0700

    Tons of changes
    I made things more effective and added some things.

--- main.py
+++ main.py
@@ -1,43 +1,61 @@
-numStudents = 5
-#creates an empty list
+import json
+import random
+import names
+fileName = 'students.json'
 studentList = []
 #adds a number to the list and adds as much as the user inputs
 #can replace items later, they are just placeholders for now
 
+# Important data that will be used in the code "I think this should be able to be changed by input" -Jacob
+_schoolData = {
+    'numStudents': 10, # How many students
+}
 
-for x in range(0, numStudents):
-    studentList.append(x)
-print(studentList)
+# Adds students to the json by ammount choosen in chooseData function
+# i is the number the student is
+for i in range(_schoolData.get('numStudents')):
+    # Chooses if student is going to be a male or female by random boolean value
+    genderBool = random.getrandbits(1)
+    if genderBool == True:
+        _studentFirstName = names.get_first_name(gender='male')
+        _studentGender = "male"
+    else:
+        _studentFirstName = names.get_first_name(gender='female')
+        _studentGender = "female"
+    _studentLastName = names.get_last_name()
+    _studentId = "10" + str(i)
+    _studentPoints = 0
+    # Turns student data into a dictionary
+    __student_data = {
+                'firstName': _studentFirstName,
+                'lastName': _studentLastName,
+                'gender': _studentGender,
+                'studentId': _studentId,
+                'studentPoints': _studentPoints
+            }   
+    # Opens the json file
+    with open(fileName,'r+') as file:
+        # Adds the student to Json file
+        fileData = json.load(file)
+        fileData["students"].append(__student_data)
+        file.seek(0)
+        json.dump(fileData, file, indent = 4)
 
-class student:
-      
-   def __init__(self, name, grade, points):
-    self.name = name
-    self.grade = grade
-    self.points = points
-
-s1 = student('HARRY', 'A', 12)
-s2 = student('Johnny', 'C', 6)
-s3 = student('Bob', 'B', 3)
-s4 = student('Ryan', 'D', 49)
-
-Points = [s1.points, s2.points, s3.points, s4.points]
-Best = 0
-SecondBest = 0
-ThirdBest = 0
-FourthBest = 0
-for x in Points:
-    if x > Best:
-        Best = x
-for y in Points:
-    if y > SecondBest and y != Best:
-       SecondBest = y
-for z in Points:
-    if z > ThirdBest and z != Best and z != SecondBest:
-        ThirdBest = z
-
-
-print(Best, SecondBest, ThirdBest)
+# Counts and orders who has the most points and returns: 
+# [[studentId, studentPoints],[studentId, studentPoints],[studentId, studentPoints],[studentId, studentPoints]]
+# The first place starts on the left and moves to the right
+def pointsLeaderboard():
+    """Function that returns the top of the leaderboard."""
+    _Leaderboard = []
+    with open('students.json', 'r') as file:
+            fileData = json.load(file)
+    Leaderboard = fileData["students"]
+    # Sorts studentPoints from greatest to least then sorts the leaderboard by [studentId, studentPoints]
+    Leaderboard.sort(key=lambda x: x["studentPoints"],reverse=True)
+    for i in range(_schoolData.get("numStudents")):
+        _Leaderboard.append([Leaderboard[i]['studentId'],Leaderboard[i]['studentPoints']])
+    return _Leaderboard
+leaderboard = pointsLeaderboard()
 
 
 #ignore this for now, I am asking for help from Mr. Bernard

2a9949d3ddc5247c72d508cd6de45dadf0b90b3e
ContriverJake <77342747+JRigney6993-coder@users.noreply.github.com>
2022-08-20 14:53:58 -0700

    just a quick ui change
    changed a couple small things

--- ui.py
+++ ui.py
@@ -1,19 +1,13 @@
 from tkinter import *
 import main
 root = Tk()
+root.title("School control panel")
 
 # Functions
 def _createStudents():
-
     main.createStudents(int(studentsNum.get()))
-    myLabel = Label(root, text = "Added " + studentsNum.get() + " Students").grid(row=5, column=0)
+    Label(root, text = "Added " + studentsNum.get() + " Students").grid(row=5, column=0)
     #myLabel.pack()
-
-
-
-# Start
-Label(root, text = "School control panel").grid(row=1, column=0)
-Label(root).grid(row=2)
 
 # Choose how many students
 Label(root, text = "Add students:").grid(row=3, column=0)

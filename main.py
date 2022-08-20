a5caebb0c587167d7826b8491af5af6ea3bc172a
Dearborn42 <amurf26@outlook.com>
2022-08-19 18:19:22 -0700

    ANdrew
    stufff

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-print("test")
 
-class Person:
-  def __init__(self, name, grade, points):
+class student:
+      
+   def __init__(self, name, grade, points):
     self.name = name
     self.grade = grade
     self.points = points
 
-  def myfunc(x):
-    print(x.name, x.grade, x.points)
+s1 = student('HARRY', 'A', 5)
+s2 = student('Johnny', 'C', 7)
+
+Points = [s1.points, s2.points]
+Best = 0
 
 
-p1 = Person("John", 'B', 3)
-p2 = Person("Joe", 'A', 6)
-
-Person.myfunc(p1)
-Person.myfunc(p2)
+print(s1.name)
+print(s2.grade)

f8bb3ea1d702f0056977dc9ff33eeb13621eedd5
Andrew <324737364@student.guhsdaz.org>
2022-08-24 13:35:21 -0700

    Got rid of old timer

--- ui.py
+++ ui.py
@@ -80,17 +80,4 @@ root.mainloop()
 # import the time module
 
 
-def countdown(t):
 
-    while t:
-        mins, secs = divmod(t, 60)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(timer, end="\r")
-        time.sleep(1)
-        t += 1
-        if t == 120:
-            t = 1
-
-
-t = 1
-#countdown(int(t))

874af9ca66b5c750a590368b037e02f8a72b1b6d
Andrew <324737364@student.guhsdaz.org>
2022-08-24 20:22:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 from asyncio.windows_events import NULL
-import random
-import names
-import json
-import time
-import math
+import random,names,json,time,math
 
 fileName = 'students.json'
-
 
 def createStudents(numStudents):
     global studentsNum
@@ -24,22 +19,23 @@ def createStudents(numStudents):
         _studentId = "10" + str(i)
 
         # Random grade level: 0 is freshmen - 3 is senior
-        gradeLevel = random.choice([9, 10, 11, 12])
+        gradeLevel = random.choice([9,10,11,12])
 
         # Turns student data into a dictionary
         __student_data = {
-            'firstName': _studentFirstName,
-            'lastName': _studentLastName,
-            'gradeLevel': gradeLevel,
-            'gender': _studentGender,
-            'studentId': _studentId,
-            'studentGrade': 100,
-            'totalPoints': 0,
-            # [Intelligence,OnTask,WorkOnTime,Happiness] --Will be used later to calculate assignment scores
-            'personality': [random.randint(50, 100), random.randint(40, 100), random.randint(55, 100), random.randint(40, 100)],
-            # [Math,English,Science,History]
-            'subjectSkills': [random.randint(25, 100), random.randint(30, 100), random.randint(45, 100), random.randint(20, 100)]
-        }
+                    'firstName': _studentFirstName,
+                    'lastName': _studentLastName,
+                    'gradeLevel': gradeLevel,
+                    'gender': _studentGender,
+                    'studentId': _studentId,
+                    'studentGrade': 100,
+                    'totalPoints': 0,
+                    
+                    # [Intelligence,OnTask,WorkOnTime,Happiness] --Will be used later to calculate assignment scores
+                    'personality': [random.randint(50, 100),random.randint(40, 100),random.randint(55, 100),random.randint(40, 100)],
+                    # [Math,English,Science,History]
+                    'subjectSkills': [random.randint(25, 100),random.randint(30, 100), random.randint(45, 100), random.randint(20, 100)]
+                }   
 
         # Opens the json file
         with open(fileName, 'r+') as file:
@@ -50,20 +46,18 @@ def createStudents(numStudents):
             file.seek(0)
             json.dump(fileData, file, indent=4)
 
-
 def pointsLeaderboard():
     """Function that returns the top of the leaderboard"""
     _Leaderboard = []
     with open(fileName, 'r') as file:
-        fileData = json.load(file)
+            fileData = json.load(file)
     Leaderboard = fileData["students"]
-    # Sorts studentPoints from greatest to least then sorts the leaderboard by [studentId, studentPoints, studentName]
-    Leaderboard.sort(key=lambda x: x["totalScore"], reverse=True)
-    for i in range(fileData["studentsNumber"]):
-        _Leaderboard.append([Leaderboard[i]['studentId'], Leaderboard[i]['totalScore'],
-                            Leaderboard[i]['firstName'] + " " + Leaderboard[i]['lastName']])
-    return _Leaderboard
 
+    # Sorts studentPoints from greatest to least then sorts the leaderboard by [studentId, studentPoints, studentName]
+    Leaderboard.sort(key=lambda x: x["totalScore"],reverse=True)
+    for i in range(fileData["studentsNumber"]):
+        _Leaderboard.append([Leaderboard[i]['studentId'],Leaderboard[i]['totalScore'],Leaderboard[i]['firstName'] + " " + Leaderboard[i]['lastName']])
+    return _Leaderboard
 
 def assignmentCreation(subject):
     """Function that creates a new assignment"""
@@ -74,26 +68,22 @@ def assignmentCreation(subject):
         allScores = []
         for i in range(fileData["studentsNumber"]):
             # Calculates persons grade based on personality and subject skills.
-            score = random.randint(students[i]["subjectSkills"][subject], 101)
+            score = random.randint(students[i]["subjectSkills"][subject],101)
             personality = students[i]["personality"]
-            score = score + \
-                personality[0] + personality[1] + \
-                personality[2] + personality[3]
+            score = score + personality[0] + personality[1] + personality[2] + personality[3]
             score = score / 5
             allScores.append(score)
-            students[i]["studentGrade"] = (
-                students[i]["studentGrade"] + score) / 2
+            students[i]["studentGrade"] = (students[i]["studentGrade"] + score) / 2
         file.seek(0)
         file.truncate()
         totalScore = int(math.ceil(sum(allScores) / len(allScores)))
         json.dump(fileData, file, indent=4)
         return totalScore
 
-
 def studentEvents(event):
     """Function that creates or lets students participate in events."""
 
-    # Still working on this so ignore how some parts may not work
+    ###### Still working on this so ignore how some parts may not work
 
     sports = ["football", "soccer", "baseball", "volleyball,", "softball"]
     nonsports = ["SpellingBee", "Fbla", "ChessTournament", "ChessTournament"]
@@ -119,29 +109,3 @@ def studentEvents(event):
                         student["totalPoints"] += 1
                 elif event == "CustomEvent":
                     student["totalPoints"] += 1
-
-
-def simulation():
-    """A multi threaded function that keeps track of time and starts events when the time is reached"""
-    _time = [0, 0, 0]
-    while True:
-        time.sleep(1)
-        _time[2] += 1
-        print(_time)
-        if _time[2] % 30 == 0:
-            assignmentCreation(0)
-            assignmentCreation(1)
-            assignmentCreation(2)
-            assignmentCreation(3)
-        if _time[1] >= 5:
-            with open(fileName, 'r+') as file:
-                fileData = json.load(file)
-                students = fileData["students"]
-                for i in range(fileData["studentsNumber"]):
-                    student = students[i]
-                    student[i]["studentGrade"] = 100
-            _time[1] = 0
-        else:
-            if _time[2] >= 60:
-                _time[2] = 0
-                _time[1] += 1

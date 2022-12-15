e1b7722c9c01d0980f03d52816e4100c6cb61e25
JacobRigney <JRigney6993@gmail.com>
2022-12-14 18:11:33 -0700

    save

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import eel
 eel.init('web')
 
+
+@eel.expose
+def test():
+    return "test"
+
+
 eel.start('index.html')
--- web/script.js
+++ web/script.js
@@ -1,7 +1,12 @@
-async function getDataFromPython() {
-    document.getElementById('test').innerText = await eel.get_data()();
+// async function getDataFromPython() {
+//     document.getElementById('test').innerText = await eel.get_data()();
+// }
+
+function output(message) {
+    document.getElementById('output').innerHTML = "test";
 }
 
-document.getElementById('').addEventListener('click', () => {
-    console.log(getDataFromPython())
+document.getElementById('addStudents').addEventListener('click', () => {
+    output("test");
+    console.log(eel.test());
 })
